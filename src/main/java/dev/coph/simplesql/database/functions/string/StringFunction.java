ae85373e6a0a15c108b6cf41c122a3e15efae714
JAN MEINL <jan.meinl@mercedes-benz.com>
2025-02-21 09:36:43 +0100

    Format code for function implementations.
    Updated the formatting of placeholder function implementations across various function classes (Advanced, String, Date, and Numeric) to improve code readability and maintain consistency. No functional changes were made.

--- src/main/java/dev/coph/simplesql/database/functions/string/StringFunction.java
+++ src/main/java/dev/coph/simplesql/database/functions/string/StringFunction.java
@@ -3,90 +3,119 @@ package dev.coph.simplesql.database.functions.string;
 import dev.coph.simplesql.database.functions.Function;
 
 public class StringFunction {
-    public class Ascii implements Function {/*TODO*/
+    public class Ascii implements Function {
+        /*TODO*/
     }
 
-    public class CharacterLength implements Function {/*TODO*/
+    public class CharacterLength implements Function {
+        /*TODO*/
     }
 
-    public class CharLength implements Function {/*TODO*/
+    public class CharLength implements Function {
+        /*TODO*/
     }
 
-    public class Concat implements Function {/*TODO*/
+    public class Concat implements Function {
+        /*TODO*/
     }
 
-    public class ConcatWs implements Function {/*TODO*/
+    public class ConcatWs implements Function {
+        /*TODO*/
     }
 
-    public class Field implements Function {/*TODO*/
+    public class Field implements Function {
+        /*TODO*/
     }
 
-    public class FindInSet implements Function {/*TODO*/
+    public class FindInSet implements Function {
+        /*TODO*/
     }
 
-    public class Format implements Function {/*TODO*/
+    public class Format implements Function {
+        /*TODO*/
     }
 
-    public class Insert implements Function {/*TODO*/
+    public class Insert implements Function {
+        /*TODO*/
     }
 
-    public class Instr implements Function {/*TODO*/
+    public class Instr implements Function {
+        /*TODO*/
     }
 
-    public class Left implements Function {/*TODO*/
+    public class Left implements Function {
+        /*TODO*/
     }
 
-    public class Length implements Function {/*TODO*/
+    public class Length implements Function {
+        /*TODO*/
     }
 
-    public class Locate implements Function {/*TODO*/
+    public class Locate implements Function {
+        /*TODO*/
     }
 
-    public class Lower implements Function {/*TODO*/
+    public class Lower implements Function {
+        /*TODO*/
     }
 
-    public class LPad implements Function {/*TODO*/
+    public class LPad implements Function {
+        /*TODO*/
     }
 
-    public class LTrim implements Function {/*TODO*/
+    public class LTrim implements Function {
+        /*TODO*/
     }
 
-    public class Position implements Function {/*TODO*/
+    public class Position implements Function {
+        /*TODO*/
     }
 
-    public class Repeat implements Function {/*TODO*/
+    public class Repeat implements Function {
+        /*TODO*/
     }
 
-    public class Replace implements Function {/*TODO*/
+    public class Replace implements Function {
+        /*TODO*/
     }
 
-    public class Reverse implements Function {/*TODO*/
+    public class Reverse implements Function {
+        /*TODO*/
     }
 
-    public class Right implements Function {/*TODO*/
+    public class Right implements Function {
+        /*TODO*/
     }
 
-    public class RPad implements Function {/*TODO*/
+    public class RPad implements Function {
+        /*TODO*/
     }
 
-    public class RTrim implements Function {/*TODO*/
+    public class RTrim implements Function {
+        /*TODO*/
     }
 
-    public class Space implements Function {/*TODO*/
+    public class Space implements Function {
+        /*TODO*/
     }
 
-    public class Strmp implements Function {/*TODO*/
+    public class Strmp implements Function {
+        /*TODO*/
     }
 
-    public class SubString implements Function {/*TODO*/
+    public class SubString implements Function {
+        /*TODO*/
     }
 
-    public class SubStringIndex implements Function {/*TODO*/
+    public class SubStringIndex implements Function {
+        /*TODO*/
     }
 
-    public class Trim implements Function {/*TODO*/
+    public class Trim implements Function {
+        /*TODO*/
     }
 
-    public class Upper implements Function {/*TODO*/
+    public class Upper implements Function {
+        /*TODO*/
     }
 }
